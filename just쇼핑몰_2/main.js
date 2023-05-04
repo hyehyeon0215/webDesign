98c2ec03d40249eb77e8c2a04aa7d903d0455fc6
hyehyeon0215 <qwert0880@naver.com>
2023-05-04 09:41:32 +0900

    just 쇼핑몰 전체 완성본

---
+++ just쇼핑몰_2/main.js
@@ -0,0 +1,5 @@
+// gnb
+$('#gnb').on('mouseenter', function () { $('.sub').stop().slideDown() });
+$('#gnb').on('mouseleave', function () { $('.sub').stop().slideUp(); });
+$('#gnb>li').on("mouseenter", function () { $(this).children('a').addClass("on"); });
+$('#gnb>li').on("mouseleave", function () { $(this).children('a').removeClass('on'); });
