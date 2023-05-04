bed627067d5c04e9ec97126e435b6ca40b292919
hyehyeon0215 <qwert0880@naver.com>
2023-05-04 11:25:40 +0900

    팝업창 기능 추가

--- just쇼핑몰_2/main.js
+++ just쇼핑몰_2/main.js
@@ -47,3 +47,9 @@ setInterval(function(){
     })
 }, 3000);
 
+$("#notice>ul>li").on("click", function(){
+    $(".popup").css("display", "block");
+});
+$("#close") .click(function(){
+    $(".popup").css("display", "none");
+});
